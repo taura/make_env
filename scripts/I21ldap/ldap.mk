085b7c3171bc0e7a942a7718a7533479809a6c6c
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-10-10 18:44:39 +0900

    nfs

--- scripts/I21ldap/ldap.mk
+++ scripts/I21ldap/ldap.mk
@@ -19,6 +19,8 @@ export ldap_domain_dn = $(shell python3 -c "print(','.join([ 'dc=%s' % x for x i
 # abc.example.com -> abc
 export ldap_domain_short = $(shell python3 -c "print('$(ldap_domain)'.split('.')[0])")
 
+export ldap_server_hostname = $(call query,select hostname from hosts where is_ldap_server=1 limit 1)
+
 export ldap_domain_password = $(call gvar,ldap_domain_password)
 export ldap_domain_password_hash = $(shell slappasswd -s $(ldap_domain_password))
 
@@ -40,14 +42,14 @@ ldap_server : root_pw make_domain
 
 # ldap_root_password_hash
 # ldap_domain_dn
-root_pw : templates/change_root_pw.ldif /usr/bin/ldapadd work
+root_pw : templates/change_root_pw.ldif /usr/bin/ldapadd work/dir
 	envsubst < templates/change_root_pw.ldif > work/change_root_pw.ldif
 	ldapadd -Y EXTERNAL -H "ldapi:///" -f work/change_root_pw.ldif
 
 # ldap_domain_password_hash
 # ldap_domain_short
 # ldap_domain_dn
-make_domain : templates/make_domain.ldif /usr/bin/ldapadd work
+make_domain : templates/make_domain.ldif /usr/bin/ldapadd work/dir
 	envsubst < templates/make_domain.ldif > work/make_domain.ldif
 	ldapsearch -Y EXTERNAL -H "ldapi:///" -s base -b $(ldap_domain_dn) > /dev/null || slapadd -l work/make_domain.ldif
 
@@ -62,25 +64,25 @@ ldap_client : /etc/ldap.conf /etc/ldap.secret remove_use_authtok /etc/sssd/sssd.
 
 # ldap_domain_dn
 # ldap_server_hostname
-/etc/ldap.conf : templates/ldap.conf /usr/sbin/sssd work
+/etc/ldap.conf : templates/ldap.conf /usr/sbin/sssd work/dir
 	envsubst < templates/ldap.conf > work/ldap.conf.add
 	$(kv_merge) /etc/ldap.conf work/ldap.conf.add > work/etc_ldap.conf
 	$(instf) work/etc_ldap.conf /etc/ldap.conf
 
 # ldap_domain_password
-/etc/ldap.secret : templates/ldap.secret /usr/sbin/sssd work
+/etc/ldap.secret : templates/ldap.secret /usr/sbin/sssd work/dir
 	envsubst < templates/ldap.secret > work/ldap.secret
 	$(inst) -m 600 work/ldap.secret /etc/ldap.secret
 
-remove_use_authtok : work
+remove_use_authtok : work/dir
 	sed -e 's/^password\(.*\)use_authtok\(.*\)$$/password \1 \2/g' /etc/pam.d/common-password > work/common-password
 	$(inst) work/common-password /etc/pam.d/common-password
 
-/etc/sssd/sssd.conf : templates/sssd.conf /usr/sbin/sssd work
+/etc/sssd/sssd.conf : templates/sssd.conf /usr/sbin/sssd work/dir
 	envsubst < templates/sssd.conf > work/sssd.conf
 	$(inst) -m 600 work/sssd.conf /etc/sssd/sssd.conf
 	service sssd restart
 
-work :
+work/dir :
 	mkdir -p $@
 
---
+++ scripts/I35nfs/nfs.mk
@@ -0,0 +1,39 @@
+#
+# nfs.mk --- configure nfs server/client
+#
+
+include ../common.mk
+
+is_nfs_server:=$(call hvar,is_nfs_server)
+ifeq ($(is_nfs_server),1)
+  fstab:=templates/fstab_server
+  targets:=nfs_common fstab nfs_server exports
+else
+  fstab:=templates/fstab_client
+  targets:=nfs_common fstab
+endif
+export nfs_server_hostname = $(call query,select hostname from hosts where is_nfs_server=1 limit 1)
+export nfs_clients = $(call query,select PrivateIpAddress from hosts)
+
+OK : $(targets)
+
+nfs_common :
+	$(aptinst) nfs-common
+
+nfs_server :
+	$(aptinst) nfs-kernel-server
+
+exports : nfs_server work/dir
+	(echo -n "/home " ; for c in $(nfs_clients); do echo -n "$$c(rw,async,no_root_squash,no_subtree_check) "; done ; echo "") > work/etc_exports.add
+	$(kv_merge) /etc/exports etc_exports.add > work/etc_exports
+	$(inst) work/etc_exports /etc/exports
+	exportfs -a
+
+fstab : nfs_common $(fstab) work/dir
+	envsubst < $(fstab) > work/etc_fstab.add
+	$(kv_merge) /etc/fstab work/etc_fstab.add > work/etc_fstab
+	$(inst) work/etc_fstab /etc/fstab
+	mount -a
+
+work/dir :
+	mkdir -p $@
