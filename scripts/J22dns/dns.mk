6504d5349ed6a42e5258c60b49a22e3e437a8b89
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-10-10 21:06:07 +0900

    dns

--- scripts/J22dns/dns.mk
+++ scripts/J22dns/dns.mk
@@ -4,9 +4,10 @@
 include ../common.mk
 
 dns_name:=$(call hvar,dns_name)
-ifneq ($(dns_name),)
-  targets := dns
-#  targets :=
+noip_conf:=$(call hvar,noip_conf)
+ifneq ($(noip_conf),)
+#  targets := noip
+  targets :=
 else
   targets := 
 endif
@@ -15,8 +16,6 @@ public_ip := $(call hvar,PublicIpAddress)
 
 OK : $(targets)
 
-dns : noip
-
 noip : noip-2.1.9-1/noip2 /usr/local/etc/no-ip2.conf
 	noip-2.1.9-1/noip2 -i $(public_ip)
 
@@ -24,5 +23,6 @@ noip-2.1.9-1/noip2 : noip-duc-linux.tar.gz
 	tar xf noip-duc-linux.tar.gz
 	cd noip-2.1.9-1 && make
 
-/usr/local/etc/no-ip2.conf : 
+/usr/local/etc/no-ip2.conf : $(data_dir)/$(noip_conf)
+	$(inst) $(noip_conf) $@
 
