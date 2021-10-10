846d25bd136fb53a32c162152fffdf7c56f6119f
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-10-10 20:36:50 +0900

    fix cgroup2

--- scripts/J30cgroup2/cgroup2.mk
+++ scripts/J30cgroup2/cgroup2.mk
@@ -12,7 +12,7 @@ OK : /boot/grub/grub.cfg /usr/local/bin/cg_mem_limit
 /etc/default/grub.d/50-cloudimg-settings.cfg : templates/50-cloudimg-settings.cfg
 	$(inst) -m 644 templates/50-cloudimg-settings.cfg /etc/default/grub.d/
 
-/usr/local/bin/cg_mem_limit : src/cg_mem_limit.py /usr/local/bin/write_to_cgroup_procs work
+/usr/local/bin/cg_mem_limit : src/cg_mem_limit.py /usr/local/bin/write_to_cgroup_procs work/dir
 	sed s:WRITE_TO_CGROUP_PROCS:/usr/local/bin/write_to_cgroup_procs:g src/cg_mem_limit.py > work/cg_mem_limit
 	chmod +x work/cg_mem_limit
 	sudo install work/cg_mem_limit $@
