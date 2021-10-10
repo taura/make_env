08c1353df24e9b4d6e70fe427ff80ec845aab02d
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-10-10 18:49:26 +0900

    nfs

--- scripts/I35nfs/nfs.mk
+++ scripts/I35nfs/nfs.mk
@@ -15,6 +15,8 @@ endif
 export nfs_server_hostname = $(call query,select hostname from hosts where is_nfs_server=1 limit 1)
 export nfs_clients = $(call query,select PrivateIpAddress from hosts)
 
+$(error $(nfs_clients))
+
 OK : $(targets)
 
 nfs_common :
