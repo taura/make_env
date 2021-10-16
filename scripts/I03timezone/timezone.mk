ed0e9af24fc465dcbf5ced9e4182a0a56657034f
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-10-16 18:46:33 +0900

    timezone

--- scripts/I03timezone/timezone.mk
+++ scripts/I03timezone/timezone.mk
@@ -3,16 +3,5 @@
 # 
 include ../common.mk
 
-OK : /etc/sysconfig/clock /etc/localtime
-
-/etc/sysconfig/clock : work/dir
-	echo -e 'ZONE="Asia/Tokyo"\nUTC=false' > work/clock
-	$(instf) work/clock $@
-
-/etc/localtime : FORCE
-	ln -sf -b -S .bak /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-
-FORCE :
-
-work/dir :
-	mkdir -p $@
+OK :
+	timedatectl set-timezone Asia/Tokyo
